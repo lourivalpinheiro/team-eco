2ba6e0a9238882d7adc9a3f08e98428827a66106
Lourival Pinheiro <lourivaltpneto@outlook.com>
2025-05-09 23:24:59 -0300

    Navigation | Modal

--- model.py
+++ model.py
@@ -1,7 +1,10 @@
 # Importing necessary libraries
 import pandas as pd
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
 
-# Dataframe data
+# Data logic 
+## Dataframe data
 data = {
     "DEBITO": [],
     "CREDITO": [],
@@ -12,5 +15,11 @@ data = {
     "FILIAL": []
 }
 
-# Creating dataframe
+## Creating dataframe
 df = pd.DataFrame(data)
+
+# Google sheets api connection
+def apiConnect():
+    conn = st.connection("gsheets", type=GSheetsConnection)
+    df = conn.read(spreadsheet=st.secrets['database']['spreadsheet'])
+    return df  # ← Important!
---
+++ pages/news.py
@@ -0,0 +1,42 @@
+# Importing necessary libraries
+import streamlit as st
+
+# Dialog for feedback
+@st.dialog("FEEDBACK")
+def navigationFeedback():
+            with st.form("navigationFeedback", enter_to_submit=False):
+                name = st.text_input("Nome")
+                surname = st.text_input("Sobrenome")
+                sector = st.selectbox("Setor", ["Contábil", "Fiscal", "Financeiro", "RH", "TI"])
+                opnion = st.text_area("O que achou da nova barra de navegação?")
+                submited = st.form_submit_button("Enviar")
+                if submited:
+                    if name == "":
+                        st.warning("Preencha o campo Nome")
+                    elif surname == "":
+                        st.warning("Preencha o campo Sobrenome")
+                    elif sector == "":
+                        st.warning("Preencha o campo Setor")
+                    elif opnion == "":
+                        st.warning("Preencha o campo Opinião")
+                    else:
+                        st.info("Obrigado pelo feedback! Sua opinião é muito importante.", icon="🙏🏼")
+    
+# Page's main configuration
+st.set_page_config(page_title="News", page_icon="📰", layout="centered")
+st.markdown("# 📰 Dev News")
+st.caption("Saiba quais as principais novidades e atualizações do projeto.")
+st.divider()
+
+# News
+with st.expander(label="Barra de navegação"):
+    st.write("09/05/2025")
+    if st.button("FEEDBACK"):
+        navigationFeedback()
+        
+            
+    st.image("static/navigationBar.png")
+    st.markdown('''
+                Agora você pode navegar entre as páginas do sistema através da barra de navegação na parte superior da tela.
+                Isso facilita a navegação e torna a experiência do usuário mais intuitiva. Que tal compartilhar essa novidade com a galera?
+                ''')
